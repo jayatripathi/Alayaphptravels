2e57763794dc50fc14033e41f477fa03d441be8d
Pranshu Dwivedi <jayatripathi0611@gmail.com>
2019-02-20 10:30:45 -0500

    replaced staticWaitStatements with dynamicWaitStatements

--- src/test/java/phpTravels/stepDefinitions/HomeStepDefinitionsNew.java
+++ src/test/java/phpTravels/stepDefinitions/HomeStepDefinitionsNew.java
@@ -1,7 +1,5 @@
 package phpTravels.stepDefinitions;
 
-import java.util.concurrent.TimeUnit;
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import phpTravels.pageObjects.HomePage;
@@ -13,14 +11,8 @@ public class HomeStepDefinitionsNew extends TestBaseNew {
 
 	@Given("^Open \"([^\"]*)\" browser and start application \"([^\"]*)\"$")
 	public void open_browser_and_start_application(String browserName, String url) throws Throwable {
-
-//		driver = TestBaseNew.selectBrowser(browserName);
 		driver.manage().window().maximize();
-//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get(url);
-
-//		Thread.sleep(3000);
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 	}
 
